54747436718a4ea644fe39f05648af47ccd7f9fd
OguzCanbaz <oguzcnbzz@gmail.com>
2023-10-26 15:29:07 +0300

    DefaultView düzeltildi.

--- Application/Application/Components/DefaultView.swift
+++ Application/Application/Components/DefaultView.swift
@@ -1,13 +1,18 @@
 import UIKit
 
 class DefaultView: UIView {
+
     override init(frame: CGRect) {
-        super.init(frame: frame) 
-        self.backgroundColor = UIColor(hex: "38ada9")
+        super.init(frame: frame)
+        commonInit()
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        self.backgroundColor = UIColor(hex: "38ada9")
     }
 }
-
