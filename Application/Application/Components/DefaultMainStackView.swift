ca644d467a635441b9ab3def7fbc551a834fe05c
OguzCanbaz <oguzcnbzz@gmail.com>
2023-10-26 15:29:33 +0300

    DefaultMainStackView düzeltildi.

--- Application/Application/Components/DefaultMainStackView.swift
+++ Application/Application/Components/DefaultMainStackView.swift
@@ -1,17 +1,23 @@
 import UIKit
 
 class DefaultMainStackView: UIStackView {
-    
+
     override init(frame: CGRect) {
-        super.init(frame: frame) 
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
         self.axis = .vertical
         self.backgroundColor = UIColor(hex: "F8F8F8")
         self.layer.cornerRadius = 80
+        self.layoutIfNeeded()
         self.layer.maskedCorners = [.layerMinXMinYCorner]
         self.distribution = .fillProportionally
-    }
-    
-    required init(coder: NSCoder) {
-        super.init(coder: coder)
     }
 }
