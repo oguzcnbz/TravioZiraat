2c78a7136260a00810db230824cd01893821ff85
OguzCanbaz <oguzcnbzz@gmail.com>
2023-11-03 10:53:51 +0300

    HomeDetailPlacesCell oluşturuldu.

---
+++ Application/Application/features/placeDetail/HomeDetailPlacesCellVC.swift
@@ -0,0 +1,116 @@
+
+import UIKit
+import SnapKit
+
+class HomeDetailPlacesCell: UICollectionViewCell {
+
+    weak var delegate:DataTransferDelegate?
+    
+    var closure:(()->Void)?
+    
+    private lazy var imgPlace:UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+
+    private lazy var ViewCell:UIView = {
+        let v = UIView()
+        v.backgroundColor = .white
+        v.layer.cornerRadius = 16
+        v.layer.borderColor = UIColor.black.cgColor
+        v.layer.shadowOpacity = 0.15
+        v.layer.shadowRadius = 20
+        
+        return v
+    }()
+    
+    
+    private lazy var vector:UIImageView = {
+        let img = UIImageView()
+        img.image = UIImage(named: "icVisitBlack")
+        return img
+    }()
+    
+    private lazy var lblName:UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = UIColor(hex: "3D3D3D")
+        lbl.font = FontStyle.poppinsSemiBold(size: 24).font
+        return lbl
+    }()
+    
+    private lazy var lblPlace:UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = UIColor(hex: "3D3D3D")
+        lbl.font = FontStyle.poppinsLight(size: 16).font
+        return lbl
+    }()
+    
+   
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    
+    @objc func btnTapped(){
+        closure?()
+        delegate?.getData(data: "")
+    }
+    
+    public func configure(object:PlacesModel){
+        imgPlace.image = object.image
+        lblName.text = object.name
+        lblPlace.text = object.place
+        
+    }
+    
+    private func setupViews(){
+        self.contentView.backgroundColor = UIColor(hex: "F8F8F8")
+        self.contentView.addSubviews(ViewCell)
+        ViewCell.addSubviews(imgPlace,lblName,vector,lblPlace)
+        
+        setupLayout()
+    }
+    
+    private func setupLayout(){
+        
+        ViewCell.snp.makeConstraints({v in
+            v.leading.equalToSuperview()
+            v.top.equalToSuperview()
+            v.bottom.equalToSuperview()
+            v.trailing.equalToSuperview()
+        })
+        
+        imgPlace.snp.makeConstraints({ imgv in
+            imgv.leading.equalToSuperview()
+            imgv.top.equalToSuperview()
+            imgv.bottom.equalToSuperview()
+            imgv.width.equalTo(90)
+            
+        })
+        
+        lblName.snp.makeConstraints({lbl in
+            lbl.leading.equalTo(imgPlace.snp.trailing).offset(8)
+            lbl.top.equalTo(ViewCell.snp.top).offset(16)
+        })
+        
+        vector.snp.makeConstraints({imgv in
+            imgv.leading.equalTo(imgPlace.snp.trailing).offset(8)
+            imgv.top.equalTo(lblName.snp.bottom).offset(3)
+        })
+        
+        lblPlace.snp.makeConstraints({lbl in
+            lbl.leading.equalTo(vector.snp.trailing).offset(6)
+            lbl.centerY.equalTo(vector)
+        })
+       
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
