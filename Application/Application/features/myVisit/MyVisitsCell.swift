931f4dfbe9f48cf40eaf45ec5444e10ecffc128b
OguzCanbaz <oguzcnbzz@gmail.com>
2023-11-03 10:56:22 +0300

    Myvisitcell üzerinde değişiklikler yapıldı.

--- Application/Application/features/myVisit/MyVisitsCell.swift
+++ Application/Application/features/myVisit/MyVisitsCell.swift
@@ -11,36 +11,31 @@ class MyVisitsCell: UICollectionViewCell {
     
     private lazy var imgPlace:UIImageView = {
         let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit
+        iv.contentMode = .scaleToFill
+        iv.layer.borderColor = UIColor.black.cgColor
+        iv.layer.shadowOpacity = 0.15
+        iv.layer.shadowRadius = 20
         return iv
     }()
-    
-    private lazy var imgStackview:UIStackView = {
-        let stack = UIStackView()
-        stack.layer.cornerRadius = 16
-        stack.layer.borderColor = UIColor.black.cgColor
-        stack.layer.shadowOpacity = 0.15
-        stack.layer.shadowRadius = 20
-        return stack
-    }()
+
     
     private lazy var vector:UIImageView = {
         let img = UIImageView()
-        img.image = UIImage(named: "icVisit-White")
+        img.image = UIImage(named: "icVisitWhite")
         return img
     }()
     
     private lazy var lblName:UILabel = {
         let lbl = UILabel()
         lbl.textColor = UIColor(hex: "FFFFFF")
-        lbl.font = UIFont(name: "Poppins-SemiBold", size: 30)
+        lbl.font = FontStyle.poppinsSemiBold(size: 30).font
         return lbl
     }()
     
     private lazy var lblPlace:UILabel = {
         let lbl = UILabel()
         lbl.textColor = UIColor(hex: "FFFFFF")
-        lbl.font = UIFont(name: "Poppins-Light", size: 16)
+        lbl.font = FontStyle.poppinsLight(size: 16).font
         return lbl
     }()
     
@@ -65,35 +60,31 @@ class MyVisitsCell: UICollectionViewCell {
     }
     
     private func setupViews(){
+        self.contentView.backgroundColor = UIColor(hex: "F8F8F8")
         self.contentView.addSubviews(imgPlace)
-        imgPlace.addSubview(imgStackview)
-        imgStackview.addSubviews(lblName,vector,lblPlace)
+        imgPlace.addSubviews(lblName,vector,lblPlace)
+        
         setupLayout()
     }
     
     private func setupLayout(){
         
-        imgPlace.snp.makeConstraints({ image in
-            image.leading.equalToSuperview()
-            image.top.equalToSuperview()
-            image.bottom.equalToSuperview()
-        })
-        
-        imgStackview.snp.makeConstraints({image in
-            image.leading.equalTo(imgPlace.snp.leading)
-            image.trailing.equalTo(imgPlace.snp.trailing)
-            image.bottom.equalTo(imgPlace.snp.bottom)
-            image.top.equalTo(imgPlace.snp.top).offset(110)
+        imgPlace.snp.makeConstraints({ img in
+            img.leading.equalToSuperview()
+            img.trailing.equalToSuperview()
+            img.top.equalToSuperview()
+            img.bottom.equalToSuperview()
+            
         })
         
         lblName.snp.makeConstraints({lbl in
-            lbl.leading.equalTo(imgStackview.snp.leading).offset(8)
+            lbl.leading.equalTo(imgPlace.snp.leading).offset(8)
             lbl.bottom.equalTo(vector.snp.top).offset(2)
         })
         
         vector.snp.makeConstraints({img in
-            img.leading.equalTo(imgStackview.snp.leading).offset(8)
-            img.bottom.equalTo(imgStackview.snp.bottom).offset(-20)
+            img.leading.equalTo(imgPlace.snp.leading).offset(8)
+            img.bottom.equalTo(imgPlace.snp.bottom).offset(-20)
         })
         
         lblPlace.snp.makeConstraints({lbl in
@@ -108,3 +99,5 @@ class MyVisitsCell: UICollectionViewCell {
     }
     
 }
+
+
