6bb69c4237bccc5f557b47ec6e9d1743ae240335
OguzCanbaz <oguzcnbzz@gmail.com>
2023-11-06 11:59:59 +0300

    MapVC, MapCell ve MapViewModel oluşturuldu.

---
+++ Application/Application/features/map/MapAddPlaceVC.swift
@@ -0,0 +1,29 @@
+//
+//  MapAddPlaceVC.swift
+//  Application
+//
+//  Created by Oğuz Canbaz on 6.11.2023.
+//
+
+import UIKit
+
+class MapAddPlaceVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
