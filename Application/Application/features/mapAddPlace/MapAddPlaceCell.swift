0202a1ead6f14134ec0ffdea4942f52b3e463e29
barisAdaliDag <barisadali.dag@hotmail.com>
2023-11-14 11:10:56 +0300

    map add image bolumu eklendi

--- Application/Application/features/mapAddPlace/MapAddPlaceCell.swift
+++ Application/Application/features/mapAddPlace/MapAddPlaceCell.swift
@@ -4,6 +4,9 @@ import SnapKit
 
 class MapAddPlaceCell: UICollectionViewCell {
     
+    var closure:(()->Void)?
+    var hasImage:Bool = false
+    
     
     private lazy var imgPlace:UIImageView = {
         let iv = UIImageView()
@@ -30,7 +33,19 @@ class MapAddPlaceCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(cellTapped))
+                self.contentView.addGestureRecognizer(tapGesture)
+                self.contentView.isUserInteractionEnabled = true
+        
     }
+    @objc private func cellTapped() {
+            // Add your desired action when the cell is tapped
+            print("Cell tapped!")
+        showChooseSourceTypeAlertController()
+        }
+    
+ 
+    
     
     private func setupViews(){
         self.contentView.backgroundColor = .white
@@ -41,6 +56,7 @@ class MapAddPlaceCell: UICollectionViewCell {
         self.contentView.layer.shadowRadius = 20
         
         self.contentView.addSubviews(imgPlace)
+        
         imgPlace.addSubviews(vector,addPhotolbl)
         
         setupLayout()
@@ -71,4 +87,54 @@ class MapAddPlaceCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
+}
+extension MapAddPlaceCell: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func showChooseSourceTypeAlertController() {
+        let photoLibraryAction = UIAlertAction(title: "Choose a Photo", style: .default) { (action) in
+            self.showImagePickerController(sourceType: .photoLibrary)
+        }
+        let cameraAction = UIAlertAction(title: "Take a New Photo", style: .default) { (action) in
+            self.showImagePickerController(sourceType: .camera)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        AlertService.showAlert(style: .actionSheet, title: nil, message: nil, actions: [photoLibraryAction, cameraAction, cancelAction], completion: nil)
+    }
+    
+    func showImagePickerController(sourceType: UIImagePickerController.SourceType) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        imagePickerController.sourceType = sourceType
+        UIViewController.topViewController()?.present(imagePickerController, animated: true, completion: nil)
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            self.imgPlace.image = editedImage.withRenderingMode(.alwaysOriginal)
+        } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imgPlace.image = originalImage.withRenderingMode(.alwaysOriginal)
+            
+        }
+        self.addPhotolbl.text = ""
+        self.vector.image = nil
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension UIViewController {
+    class func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
+            }
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
+    }
 }
