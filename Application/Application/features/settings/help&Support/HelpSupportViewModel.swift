68a0150af00af24af6338481d40019ed76002cae
barisAdaliDag <barisadali.dag@hotmail.com>
2023-11-29 10:55:56 +0300

    helpsupport view model eklendi

--- Application/Application/features/settings/help&Support/HelpSupportViewModel.swift
+++ Application/Application/features/settings/help&Support/HelpSupportViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+class HelpSupportViewModel { 
+    var cells:[HelpAndSupportModel] = [HelpAndSupportModel(questionLbl: "How can I create a new account on Travio?", answerLbl: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
+                                       HelpAndSupportModel(questionLbl: "How can I create a new account on Travio?", answerLbl: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
+                                       HelpAndSupportModel(questionLbl: "How does Travio work?", answerLbl: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
+                                       HelpAndSupportModel(questionLbl: "How does Travio work?", answerLbl: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
+                                       HelpAndSupportModel(questionLbl: "How does Travio work?", answerLbl: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
+                                       HelpAndSupportModel(questionLbl: "How does Travio work?", answerLbl: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")]
+
+
+    func getArr()-> [HelpAndSupportModel]{
+        return cells
+        
+    }
+}
+
+
+
